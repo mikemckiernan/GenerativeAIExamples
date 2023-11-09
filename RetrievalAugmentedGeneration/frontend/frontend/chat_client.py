193c46b6cafcbca02e84d043a356f4c104eb0a49
dharmendrac <dharmendrac@nvidia.com>
2023-11-09 14:00:42 +0530

    Latest Nemo FM inference container, openai scheam and SAST fixes

--- RetrievalAugmentedGeneration/frontend/frontend/chat_client.py
+++ RetrievalAugmentedGeneration/frontend/frontend/chat_client.py
@@ -91,5 +91,5 @@ class ChatClient:
             )
 
             _ = requests.post(
-                url, headers=headers, files=files, verify=False, timeout=30  # type: ignore [arg-type]
-            )  # nosec # verify=false is intentional for now
+                url, headers=headers, files=files, timeout=30  # type: ignore [arg-type]
+            )
